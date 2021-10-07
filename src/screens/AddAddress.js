4d9d08e4e4ce8dcbc34ec3dbf901bb3c9bb2cff2
pamudu-ratnayake <it19993416@my.sliit.lk>
2021-10-07 13:14:06 +0530

    Structure CHange

---
+++ src/screens/AddAddress.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  View,
+  Image,
+  Text,
+  Card,
+  StyleSheet,
+  Button,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+
+const AddAddress = () => {
+
+
+    return(
+
+        <ScrollView>
+
+        </ScrollView>
+        
+    )
+}
+
+export default AddAddress;
