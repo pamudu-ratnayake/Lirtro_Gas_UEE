5d9c506f23b32aeff97dc749c6f8fa7c18c67349
pamudu-ratnayake <it19993416@my.sliit.lk>
2021-10-10 17:18:24 +0530

    change navihations

--- src/components/HomeStack.js
+++ src/components/HomeStack.js
@@ -11,6 +11,11 @@ import CartItem from '../screens/cartItem';
 import HomeDealer from '../screens/homeDealer';
 import SelectDealer from '../screens/selectDealer';
 import AddAddress from '../screens/AddAddress';
+import OrderDealer from '../screens/orderDealer';
+import ProfileDealer from '../screens/profileDealer';
+import MyProfile from '../screens/myProfile';
+import Login from '../screens/login';
+import Register from '../screens/register';
 
 const switchNavigator = createSwitchNavigator({
   mainFlow: createMaterialBottomTabNavigator(
@@ -33,9 +38,25 @@ const switchNavigator = createSwitchNavigator({
           AddAddress: {
             screen: AddAddress,
             navigationOptions: {
-              headerTitle: () => <Header title="LITRO GAS" />,
+              headerTitle: () => <Header title="ADD ADDRESS" />,
             },
           },
+          OrderDealer: {
+            screen : OrderDealer,
+            headerTitle: () => <Header title="LITRO GAS" />,
+          },
+          ProfileDealer:{
+            screen: ProfileDealer,
+            headerTitle: () => <Header title="LITRO GAS" />,
+          },
+          LogInUI: {
+            screen: Login,
+            headerTitle: () => <Header title="LITRO GAS" />,
+          },
+          Register: {
+            screen: Register,
+            headerTitle: () => <Header title="LITRO GAS" />,
+          }
         },
         {
           defaultNavigationOptions: {
@@ -58,9 +79,9 @@ const switchNavigator = createSwitchNavigator({
         },
       },
       Login: {
-        screen: SelectDealer,
+        screen: MyProfile,
         navigationOptions: {
-          tabBarLabel: 'Home',
+          tabBarLabel: 'Profile',
           tabBarColor: '#2553A8',
           tabBarIcon: ({color}) => (
             <Icon name="ios-home" color={color} size={26} />
