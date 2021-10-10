c4c4ee106d329f50645ebe611a081a3496effe34
pamudu-ratnayake <it19993416@my.sliit.lk>
2021-10-10 17:19:36 +0530

    Add to navigation

---
+++ src/screens/orderDealer.js
@@ -0,0 +1,107 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ImageBackground,
+  Picker,
+  TouchableOpacity,
+} from 'react-native';
+
+// const image = require('../assets/img/background.png');
+
+const OrderDealer = () => {
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <View style={styles.container}>
+        {/* <ImageBackground source={image} style={styles.image} resizeMode="cover"> */}
+          <ScrollView>
+            <View style={styles.cardView}>
+
+              <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardText}>Pending Orders</Text>
+                {/* <Image
+                    style={styles.iconImage}
+                    source={require('../assets/img/icons/outline_arrow_forward_ios_white_24dp.png')}
+                  /> */}
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardText}>Accepted Orders</Text>
+                {/* <Image
+                    style={styles.iconImage}
+                    source={require('../assets/img/icons/outline_arrow_forward_ios_white_24dp.png')}
+                  /> */}
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardText}>Dispatched Orders</Text>
+                {/* <Image
+                    style={styles.iconImage}
+                    source={require('../assets/img/icons/outline_arrow_forward_ios_white_24dp.png')}
+                  /> */}
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.card}>
+                <Text style={styles.cardText}>Delivered Orders</Text>
+                {/* <Image
+                    style={styles.iconImage}
+                    source={require('../assets/img/icons/outline_arrow_forward_ios_white_24dp.png')}
+                  /> */}
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        {/* </ImageBackground> */}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  cardView: {
+    paddingVertical: 40,
+    paddingHorizontal: 15,
+  },
+  card: {
+    flex: 0.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    // paddingVertical: 5,
+    backgroundColor: '#009dfe',
+    borderRadius: 5,
+    marginHorizontal: 18,
+    marginVertical: 30,
+  },
+  cardText: {
+    // flex: 0.9,
+    paddingRight: 10,
+    paddingVertical: 20,
+    fontSize: 28,
+    color: '#fff',
+  },
+  // iconImage: {
+  //   flex: 0.1,
+  //   width: 25,
+  //   height: 25,
+  //   paddingVertical: 20,
+  //   marginVertical: 20,
+  //   marginRight: 5,
+  // },
+});
+
+export default OrderDealer;
---
+++ src/screens/profileDealer.js
@@ -0,0 +1,159 @@
+import React from 'react';
+// import {useState} from 'react/cjs/react.development';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ImageBackground,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+// import StarRating from 'react-native-star-rating';
+
+// const image = require('../assets/img/background.png');
+
+const ProfileDealer = () => {
+  // const [starCount, setState] = useState(3.5);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}>
+      <View style={styles.container}>
+        {/* <ImageBackground source={image} style={styles.image} resizeMode="cover"> */}
+          <ScrollView>
+            <View style={styles.pageView}>
+              <Image
+                style={styles.profImage}
+                source={require('../assets/images/dealerProfile.png')}
+              />
+              {/* <View style={styles.dealerInfor}> */}
+              <View style={styles.dealerInfor}>
+                <Text style={styles.text1}>Shop </Text>
+                <Text style={styles.text2}>MINROSE GAS SHOP </Text>
+              </View>
+
+              <View style={styles.dealerInfor}>
+                <Text style={styles.text1}>Owner </Text>
+                <Text style={styles.text2}>Milan Fernando </Text>
+              </View>
+
+              <View style={styles.dealerInfor}>
+                <Text style={styles.text1}>Tell </Text>
+                <Text style={styles.text2}>077 4849423 </Text>
+              </View>
+
+              <View style={styles.dealerInfor}>
+                <Text style={styles.text1}>Address </Text>
+                <Text style={styles.text2}>
+                  No. 56, Temple Road, Kottawa, Pannipitiya{' '}
+                </Text>
+              </View>
+              {/* </View> */}
+
+              {/* <View style={styles.viewRating}>
+                <StarRating
+                  disabled={false}
+                  maxStars={5}
+                  rating={starCount}
+                  // selectedStar={(rating) => onStarRatingPress(rating)}
+                />
+              </View> */}
+
+              <View style={styles.btn}>
+                <View style={styles.btn1}>
+                  <TouchableOpacity>
+                    <Text style={styles.btnText}>Reviews</Text>
+                  </TouchableOpacity>
+                </View>
+
+                <View style={styles.btn2}>
+                  <TouchableOpacity>
+                    <Text style={styles.btnText}>Edit</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          </ScrollView>
+        {/* </ImageBackground> */}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pageView: {
+    alignItems: 'center',
+    paddingVertical: 40,
+    paddingHorizontal: 15,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  profImage: {
+    width: 170,
+    height: 170,
+    borderRadius: 90,
+    marginBottom: 20,
+  },
+  dealerInfor: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginVertical: 5,
+    marginHorizontal: 25,
+  },
+  text1: {
+    flex: 0.3,
+    paddingVertical: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text2: {
+    flex: 0.7,
+    paddingVertical: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  viewRating: {
+    marginTop: 70
+  },
+  btn: {
+    flexDirection: 'row',
+    marginTop: 40,
+  },
+  btn1: {
+    alignItems: 'center',
+    // marginVertical: 10,
+    marginHorizontal: 30,
+    padding: 10,
+    backgroundColor: '#009dfe',
+    borderRadius: 15,
+    width: 150,
+  },
+  btn2: {
+    alignItems: 'center',
+    // marginVertical: 10,
+    marginHorizontal: 30,
+    padding: 10,
+    backgroundColor: '#009dfe',
+    borderRadius: 15,
+    width: 150,
+  },
+  btnText: {
+    fontSize: 20,
+    color: '#fff',
+  },
+});
+
+export default ProfileDealer;
